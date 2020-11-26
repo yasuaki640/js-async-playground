03f8018fc381cf295a876d7cc73cbffd63ed4e3a
yasuaki640 <yasuaki640@gmail.com>
2020-11-26 21:41:13 +0900

    Put together the lines to import the library

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const express = require('express');
-
 const dotenv = require('dotenv');
-dotenv.config({path: './.env'});
-
 const NewsAPI = require('newsapi');
+
+dotenv.config({path: './.env'});
 const newsapi = new NewsAPI(process.env.NEWS_API_ACCESS_KEY);
