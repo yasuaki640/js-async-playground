19e121b53c3d739b6e1d1217b7941b030db14d5c
yasuaki640 <yasuaki640@gmail.com>
2020-11-26 21:39:11 +0900

    Insert a blank line for each lib

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const express = require('express');
+
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
+
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(process.env.NEWS_API_ACCESS_KEY);
