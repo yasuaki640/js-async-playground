d9e7f39c31080fd105c094bb71e73e4b0267d34e
yasuaki640 <yasuaki640@gmail.com>
2020-11-26 22:20:49 +0900

    apiをとりあえずserver.jsにべた書き

--- server.js
+++ server.js
@@ -4,5 +4,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const NewsAPI = require('newsapi');
 
+const app = express();
+
 dotenv.config({path: './.env'});
 const newsapi = new NewsAPI(process.env.NEWS_API_ACCESS_KEY);
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.get("/data", (req, res) => {
+    newsapi.v2.topHeadlines({
+        country: 'jp',
+        category: 'technology',
+        pageSize: 40
+    }).then(news => res.json(news));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('listening on port ' + PORT));
