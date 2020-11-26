ec432dba7b5615cbebf03cc1f39873973d4a4d51
yasuaki640 <yasuaki640@gmail.com>
2020-11-26 21:37:15 +0900

    import libs

---
+++ server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWS_API_ACCESS_KEY);
