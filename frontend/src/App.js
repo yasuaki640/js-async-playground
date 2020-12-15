b3999f032a5c4ec69d864040cd90f6307b77cf0d
yasuaki640 <yasuaki640@gmail.com>
2020-12-15 20:20:45 +0900

    add category select

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,22 @@ export default function App() {
         {value: 'jp', label: 'jp'},
         {value: 'cn', label: 'cn'}
     ];
+    const categoryOptions = [
+        {value: 'business', label: 'business'},
+        {value: 'entertainment', label: 'entertainment'},
+        {value: 'general', label: 'general'},
+        {value: 'health', label: 'health'},
+        {value: 'science', label: 'science'},
+        {value: 'sports', label: 'sports'},
+        {value: 'technology', label: 'technology'}
+    ];
+
     return (
         <div className="App">
             <h1>Search Your Favorite Newssssss!!!!!!</h1>
             <form>
                 <Select options={countryOptions}/>
+                <Select options={categoryOptions}/>
             </form>
         </div>
     );
