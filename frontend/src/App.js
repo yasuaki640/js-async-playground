5b339da9ad36362f20b3b32ee0170cecceabc571
yasuaki640 <yasuaki640@gmail.com>
2020-12-15 20:20:25 +0900

    add select

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Select from 'react-select';
 import "./styles.css";
 
 export default function App() {
+    const countryOptions = [
+        {value: 'en', label: 'en'},
+        {value: 'jp', label: 'jp'},
+        {value: 'cn', label: 'cn'}
+    ];
     return (
         <div className="App">
             <h1>Search Your Favorite Newssssss!!!!!!</h1>
+            <form>
+                <Select options={countryOptions}/>
+            </form>
         </div>
     );
 }
