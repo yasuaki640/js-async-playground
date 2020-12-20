30612fbe0955fc1f131246995a1b3ea27a1c0e1b
yasuaki640 <yasuaki640@gmail.com>
2020-12-20 18:23:06 +0900

    フォーマッタ×

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import {SearchForm} from "./components/SearchForm";
 
 
 function App() {
-  return (
-      <div className='App'>
-        <h1>Search Your Favorite Newssssss!!!!!!</h1>
-        <p>Enter news info you looking for.</p>
-          <SearchForm/>
-      </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Search Your Favorite Newssssss!!!!!!</h1>
+            <p>Enter news info you looking for.</p>
+            <SearchForm/>
+        </div>
+    );
 }
 
 export default App;
