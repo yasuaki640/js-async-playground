d0d64f2625910d6338943e7f9b0ae3febf195922
yasuaki640 <yasuaki640@gmail.com>
2020-12-21 22:59:16 +0900

    画像を表示させ、スタイルをととのうぇら

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const imgStyle = {
+    width: '100%',
+    height: 'auto',
+    maxWidth: '400px'
+};
+
 export const Article = (props) => {
     return (
         <>
             <h4>{props.article.title}</h4>
+            <a href={props.article.url} target="_blank" rel="noopener noreferrer">
+                <img style={imgStyle} alt={props.article.title} src={props.article.urlToImage}/>
+            </a>
         </>
     );
 };
