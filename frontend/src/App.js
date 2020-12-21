c1c1e2d2641f26bb93cb50e851e69cce0e8fa8dd
yasuaki640 <yasuaki640@gmail.com>
2020-12-21 22:32:00 +0900

    子に状態を持たせない

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,87 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 import {SearchForm} from "./components/SearchForm";
-
+import axios from "axios";
 
 function App() {
+    const countryOptions = [
+        {value: 'en', label: 'en'},
+        {value: 'jp', label: 'jp'},
+        {value: 'cn', label: 'cn'}
+    ];
+    const [country, setCountry] = useState(countryOptions[0]);
+    const handleCountryChange = (country) => {
+        setCountry(country);
+    };
+
+    const categoryOptions = [
+        {value: 'business', label: 'business'},
+        {value: 'entertainment', label: 'entertainment'},
+        {value: 'general', label: 'general'},
+        {value: 'health', label: 'health'},
+        {value: 'science', label: 'science'},
+        {value: 'sports', label: 'sports'},
+        {value: 'technology', label: 'technology'}
+    ];
+    const [category, setCategory] = useState(categoryOptions[0]);
+    const handleCategoryChange = (category) => {
+        setCategory(category);
+    };
+
+    const pageSizeOptions = [
+        {value: 15, label: 15},
+        {value: 30, label: 30},
+        {value: 50, label: 50},
+    ];
+    const [pageSize, setPageSize] = useState(pageSizeOptions[0]);
+    const handlePageSizeChange = (pageSize) => {
+        setPageSize(pageSize);
+    };
+
+    const [keyword, setKeyword] = useState('');
+    const handleKeywordChange = (event) => {
+        setKeyword(event.target.value)
+    };
+
+    const [articles, setArticles] = useState([]);
+    const handleSubmit = async (event) => {
+        //prevent reload by submit
+        event.preventDefault();
+
+        let articlesArr = await axios.get('http://localhost:3000/news', {
+            params: {
+                country: country.value,
+                category: category.value,
+                q: keyword,
+                pageSize: pageSize.value
+            }
+        })
+            .then(res => res.data.articles);
+
+        setArticles(articlesArr);
+    };
+
     return (
         <div className='App'>
             <h1>Search Your Favorite Newssssss!!!!!!</h1>
             <p>Enter news info you looking for.</p>
-            <SearchForm/>
+            <SearchForm
+                country={country}
+                countryOptions={countryOptions}
+                handleCountryChange={handleCountryChange}
+                category={category}
+                categoryOptions={categoryOptions}
+                handleCategoryChange={handleCategoryChange}
+                pageSize={pageSize}
+                pageSizeOptions={pageSizeOptions}
+                handlePageSizeChange={handlePageSizeChange}
+                keyword={keyword}
+                handleKeywordChange={handleKeywordChange}
+                handleSubmit={handleSubmit}
+            />
+            {articles.map(article =>
+                <p key={article.title}>{article.title}</p>
+            )}
         </div>
     );
 }
--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,99 +1,37 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Select from 'react-select';
-import axios from 'axios';
 
 export const SearchForm = (props) => {
-
-    const countryOptions = [
-        {value: 'en', label: 'en'},
-        {value: 'jp', label: 'jp'},
-        {value: 'cn', label: 'cn'}
-    ];
-    const [country, setCountry] = useState(countryOptions[0]);
-    const handleCountryChange = (country) => {
-        setCountry(country);
-    };
-
-    const categoryOptions = [
-        {value: 'business', label: 'business'},
-        {value: 'entertainment', label: 'entertainment'},
-        {value: 'general', label: 'general'},
-        {value: 'health', label: 'health'},
-        {value: 'science', label: 'science'},
-        {value: 'sports', label: 'sports'},
-        {value: 'technology', label: 'technology'}
-    ];
-    const [category, setCategory] = useState(categoryOptions[0]);
-    const handleCategoryChange = (category) => {
-        setCategory(category);
-    };
-
-    const pageSizeOptions = [
-        {value: 15, label: 15},
-        {value: 30, label: 30},
-        {value: 50, label: 50},
-    ];
-    const [pageSize, setPageSize] = useState(pageSizeOptions[0]);
-    const handlePageSizeChange = (pageSize) => {
-        setPageSize(pageSize);
-    };
-
-    const [keyword, setKeyword] = useState('');
-    const handleKeywordChange = (event) => {
-        setKeyword(event.target.value)
-    };
-
-    const [articles, setArticles] = useState([]);
-    const handleSubmit = async (event) => {
-        //prevent reload by submit
-        event.preventDefault();
-
-        let articlesArr = await axios.get('http://localhost:3000/news', {
-            params: {
-                country: country.value,
-                category: category.value,
-                q: keyword,
-                pageSize: pageSize.value
-            }
-        })
-            .then(res => res.data.articles);
-
-        setArticles(articlesArr);
-    };
-
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={props.handleSubmit}>
                 <label>Country
                     <Select
-                        value={country}
-                        onChange={handleCountryChange}
-                        options={countryOptions}
+                        value={props.country}
+                        onChange={props.handleCountryChange}
+                        options={props.countryOptions}
                     />
                 </label>
                 <label>Category
                     <Select
-                        value={category}
-                        onChange={handleCategoryChange}
-                        options={categoryOptions}
+                        value={props.category}
+                        onChange={props.handleCategoryChange}
+                        options={props.categoryOptions}
                     />
                 </label>
                 <label>page size
                     <Select
-                        value={pageSize}
-                        onChange={handlePageSizeChange}
-                        options={pageSizeOptions}
+                        value={props.pageSize}
+                        onChange={props.handlePageSizeChange}
+                        options={props.pageSizeOptions}
                     />
                 </label>
                 <label>Keyword
-                    <input type={'text'} value={keyword} placeholder={'Enter search keyword'}
-                           onChange={handleKeywordChange}/>
+                    <input type={'text'} value={props.keyword} placeholder={'Enter search keyword'}
+                           onChange={props.handleKeywordChange}/>
                 </label>
                 <input type={'submit'} value={'Submit'}/>
             </form>
-            {articles.map(article =>
-                <p key={article.title}>{article.title}</p>
-            )}
         </>
     );
 };
