edd10833f8bbfac33782e83542c866ca46a5934b
yasuaki640 <yasuaki640@gmail.com>
2020-12-22 22:41:16 +0900

    react-bootstrap入れるならbootstrapもいれとけやぼけ

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import Select from 'react-select';
+import {Form, Button} from "react-bootstrap";
+
+const formStyle = {
+    maxWidth: '600px'
+};
 
 export const SearchForm = (props) => {
     return (
         <>
-            <form onSubmit={props.handleSubmit}>
-                <label>Country
-                    <Select
-                        value={props.country}
-                        onChange={props.handleCountryChange}
-                        options={props.countryOptions}
-                    />
-                </label>
-                <label>Category
-                    <Select
-                        value={props.category}
-                        onChange={props.handleCategoryChange}
-                        options={props.categoryOptions}
-                    />
-                </label>
-                <label>Page Size
-                    <Select
-                        value={props.pageSize}
-                        onChange={props.handlePageSizeChange}
-                        options={props.pageSizeOptions}
-                    />
-                </label>
-                <label>Keyword
-                    <input type={'text'} value={props.keyword} placeholder={'Enter search keyword'}
-                           onChange={props.handleKeywordChange}/>
-                </label>
-                <input type={'submit'} value={'Submit'}/>
-            </form>
+            <Form onSubmit={props.handleSubmit}>
+                <Form.Group ControllId={"formSearchArticles"}>
+                    <Form.Label>Country
+                        <Select
+                            value={props.country}
+                            onChange={props.handleCountryChange}
+                            options={props.countryOptions}
+                        />
+                    </Form.Label>
+                    <Form.Label>Category
+                        <Select
+                            value={props.category}
+                            onChange={props.handleCategoryChange}
+                            options={props.categoryOptions}
+                        />
+                    </Form.Label>
+                    <Form.Label>Page Size
+                        <Select
+                            value={props.pageSize}
+                            onChange={props.handlePageSizeChange}
+                            options={props.pageSizeOptions}
+                        />
+                    </Form.Label>
+                    <Form.Label>Keyword
+                        <Form.Control type={"text"} value={props.keyword} placeholder={'Enter search keyword'}
+                                      onChange={props.handleKeywordChange}/>
+                    </Form.Label>
+                </Form.Group>
+                <Button variant={"primary"} type={'submit'}>Search</Button>
+            </Form>
         </>
     );
 };
