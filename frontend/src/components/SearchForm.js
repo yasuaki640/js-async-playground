7247da3acb71f01d916a1813dd9bdb60fa592237
yasuaki640 <yasuaki640@gmail.com>
2020-12-23 23:12:17 +0900

    Form.Groupを各Selectコンポーネンとに指定してあげる

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Select from 'react-select';
-import Form from "react-bootstrap/Form";
+import Container from 'react-bootstrap/Container'
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const formStyle = {
     maxWidth: '600px'
@@ -10,32 +11,40 @@ const formStyle = {
 export const SearchForm = (props) => {
     return (
         <>
-            <Form style={formStyle} onSubmit={props.handleSubmit}>
-                <Form.Group controlId={'formSearchNews'}>
-                    <Form.Label>Country</Form.Label>
-                    <Select
-                        value={props.country}
-                        onChange={props.handleCountryChange}
-                        options={props.countryOptions}
-                    />
-                    <Form.Label>Category</Form.Label>
-                    <Select
-                        value={props.category}
-                        onChange={props.handleCategoryChange}
-                        options={props.categoryOptions}
-                    />
-                    <Form.Label>Page Size</Form.Label>
-                    <Select
-                        value={props.pageSize}
-                        onChange={props.handlePageSizeChange}
-                        options={props.pageSizeOptions}
-                    />
-                    <Form.Label>Keyword</Form.Label>
-                    <Form.Control type={"text"} value={props.keyword} placeholder={'Enter search keyword'}
-                                  onChange={props.handleKeywordChange}/>
+            <Container>
+                <Form style={formStyle} onSubmit={props.handleSubmit}>
+                    <Form.Group controlId={'formSelectCountry'}>
+                        <Form.Label>Country</Form.Label>
+                        <Select
+                            value={props.country}
+                            onChange={props.handleCountryChange}
+                            options={props.countryOptions}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId={'formSelectCategory'}>
+                        <Form.Label>Category</Form.Label>
+                        <Select
+                            value={props.category}
+                            onChange={props.handleCategoryChange}
+                            options={props.categoryOptions}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId={'formSelectLabel'}>
+                        <Form.Label>Page Size</Form.Label>
+                        <Select
+                            value={props.pageSize}
+                            onChange={props.handlePageSizeChange}
+                            options={props.pageSizeOptions}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId={'formInputKeyword'}>
+                        <Form.Label>Keyword</Form.Label>
+                        <Form.Control type={"text"} value={props.keyword} placeholder={'Enter search keyword'}
+                                      onChange={props.handleKeywordChange}/>
+                    </Form.Group>
                     <Button variant={"primary"} type={'submit'}>Search</Button>
-                </Form.Group>
-            </Form>
+                </Form>
+            </Container>
         </>
     );
 };
