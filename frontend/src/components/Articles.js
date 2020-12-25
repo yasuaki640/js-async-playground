8a6933638a79ad48980f2c9106f1fe5f76ba8f40
yasuaki640 <yasuaki640@gmail.com>
2020-12-25 23:30:24 +0900

    cardcolumuns2重だったので修正

--- frontend/src/components/Articles.js
+++ frontend/src/components/Articles.js
@@ -18,17 +18,15 @@ const Article = (props) => {
     return (
         <>
             <Card>
-                <Card style={{width: '18rem'}}>
-                    <a href={props.article.url}>
-                        <Card.Img variant='top' src={props.article.urlToImage}/>
-                    </a>
-                    <Card.Body>
-                        <Card.Title>{props.article.title}</Card.Title>
-                        <Card.Text>
-                            {props.article.article}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <a href={props.article.url}>
+                    <Card.Img variant='top' src={props.article.urlToImage}/>
+                </a>
+                <Card.Body>
+                    <Card.Title>{props.article.title}</Card.Title>
+                    <Card.Text>
+                        {props.article.article}
+                    </Card.Text>
+                </Card.Body>
             </Card>
         </>
     );
