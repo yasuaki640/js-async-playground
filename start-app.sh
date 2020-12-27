c1944424119a760654725a6237bd0268925acc47
yasuaki640 <yasuaki640@gmail.com>
2020-12-27 21:53:33 +0900

    動作確認してないけどいったんコミット

---
+++ start-app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+readonly FRONTEND_SERVER_DIR_ROOT='./frontend'
+readonly BACKEND_SERVER_DIR_ROOT='./backend'
+
+config_server() {
+  cd "$1" || return 1
+  yarn install
+  cp .env.example .env
+  cd ../
+  return 0
+}
+
+handle_error() {
+  echo "This script returns non-zero exit code $?"
+  exit 1
+}
+
+config_server ${FRONTEND_SERVER_DIR_ROOT} || handle_error
+config_server ${BACKEND_SERVER_DIR_ROOT} || handle_error
+
+cd ${BACKEND_SERVER_DIR_ROOT} || handle_error
+yarn run start
+
+echo 'starting new-api-app'
